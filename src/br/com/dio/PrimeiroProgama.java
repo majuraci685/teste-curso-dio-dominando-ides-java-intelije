06e8da15e3c15044d932ac3bfdeaab430557df2a
majuraci685 <majuraci685@gmail.com>
2023-03-10 23:26:59 -0400

    first commit

---
+++ src/br/com/dio/PrimeiroProgama.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroProgama {
+    public static void main(String[] args) {
+
+        System.out.println(Gato);
+
+        Gato gato = new Gato();
+        int a = 5;
+        int b = 3;
+        System.out.println("Hello World, " + (a+b));
+    }
+}
